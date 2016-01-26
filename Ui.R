cbb8b544397a0700c9a1e7908685a4062d7c1d1b
Scott Brenstuhl <brenstsr@miamioh.edu>
2016-01-25 23:36:02 -0800

    User address input added

--- Ui.R
+++ Ui.R
@@ -2,19 +2,31 @@ library(leaflet)
 #shinyUI(pageWithSidebar(  
 shinyUI(fluidPage(  
     headerPanel("Plan Your National Parks Trip!"),  
-    sidebarLayout(
-        sidebarPanel(    
-            textInput('trip_start', 'Trip Start', value = 'user'),
-            #need to look up real max and min
-            numericInput('usr_lat', 'My Lat', 37.767019, min = -90, max = 90),
-            numericInput('usr_long', 'My Long', -122.421781, min = -140, max = 140),
-            numericInput('park_count', '# of Parks', 10, min = 3, max = 60)
+#    sidebarLayout(
+fluidRow(
+#        sidebarPanel(
+        column(4,
+               wellPanel(
+                    textInput('usr_location', 'Where do you want to start your trip?', value = '101 Forest Ave., Palo Alto, CA'),
+                    actionButton('set_start', 'Set Start', icon("refresh")),
+                    #numericInput('usr_lat', 'My Lat', 37.767019, min = -90, max = 90),
+                    #numericInput('usr_long', 'My Long', -122.421781, min = -180, max = 180),
+                    br(),
+                    br(),
+                    numericInput('park_count', '# of Parks', 10, min = 3, max = 60)
+               )
             ),
     
-        mainPanel(    
+        #mainPanel(
+        column(8,
             #textOutput("testOut"),
             leafletOutput("mymap")
         )
     ),
-    dataTableOutput('testOut')
+    fluidRow(
+        column(12,
+        dataTableOutput('testOut')
+        #tableOutput('testOut')
+    ))
+    
 ))
--- server.R
+++ server.R
@@ -1,6 +1,6 @@
-library(UsingR)
 library(leaflet)
 library(geosphere)
+library(ggmap)
 
 shinyServer(  
     function(input, output) {    
@@ -9,15 +9,25 @@ shinyServer(
         parks_data <- read.csv('data/parks.csv', check.names = FALSE, stringsAsFactors = FALSE)
         parks_data$Name <- gsub('(\\s)','_',parks_data$Name)
         
+        # To reproduce you will need to get your own google API key from the
+        # Google developers console.
+        #config <- read.csv('config.csv', stringsAsFactors = FALSE)
+        #google_key <- config$key[config$service=="Google_maps"]
+        
         reac <- reactiveValues()
+         
+        reac$usr_location <- eventReactive(input$set_start,{
+            reac$usr_loc <- geocode(input$usr_location)
+        })
         
         reac$distances <- reactive({
-            usr_distances = distHaversine(c(input$usr_long,input$usr_lat),parks_data[c('long','lat')])
+            #usr_distances = distHaversine(c(reac$usr_loc$lon,reac$usr_loc$lat),parks_data[c('long','lat')])
+            usr_distances = distHaversine(c(reac$usr_location()$lon,reac$usr_location()$lat),parks_data[c('long','lat')])
+            #usr_distances = distHaversine(c(input$usr_long,input$usr_lat),parks_data[c('long','lat')])
             distances = cbind('user'=usr_distances, distances)
             distances = rbind(distances,'user'=c(0,usr_distances))
             distances
         })
-        
         
         
         test_trip <-reactive({ 
@@ -54,22 +64,28 @@ shinyServer(
         
         #output$testOut <- renderText(as.character(reac$distances()))
         reac$parks_data <- reactive({
-            usr_data = c('user','',0,'','','', input$usr_lat, input$usr_long)
+            #usr_data = c('user','',0,'','','', reac$usr_loc$lat, reac$usr_loc$lon)
+            usr_data = c('user','',0,'','','', reac$usr_location()$lat, reac$usr_location()$lon)
             rbind(parks_data, usr_data)
         })
         
         trip_locs <- reactive({
-            curr_trip = plan_trip(test_trip(), input$trip_start)
+            curr_trip = plan_trip(test_trip(), "user")
             reac$parks_data()[reac$parks_data()$Name %in% curr_trip,]
         })
         
         
         output$testOut <-renderDataTable(as.data.frame(trip_locs()))
+        #output$testOut <-renderTable(as.data.frame(trip_locs()))
         
         output$mymap <- renderLeaflet({
             leaflet(data = trip_locs()) %>%
                 addTiles() %>%
-                addMarkers(~long, ~lat, popup = ~Name)
+                addMarkers(~long, ~lat, popup = ~Name) %>%
+                # Reactive data breaks auto-centering so this is needed
+                fitBounds(lng1 = max(trip_locs()$long),lat1 = max(trip_locs()$lat),
+                          lng2 = min(trip_locs()$long),lat2 = min(trip_locs()$lat))
+                
         })
     }      
 )
